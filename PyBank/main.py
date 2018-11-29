b065ca0cd867df6fbba193ce7aa1dad80b928868
abubekerali <abubekerali@users.noreply.github.com>
2018-11-29 14:04:41 -0500

    ..

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,61 +1,73 @@
+# -*- coding: UTF-8 -*-
+"""PyBank Homework Solution."""
+
+# Dependencies
 import csv
+import os
 
+# Files to load and output (Remember to change these)
+file_to_load = os.path.join("Resources", "budget_data.csv")
+file_to_output = os.path.join("analysis", "budget_analysis.txt")
 
-#import CSV files.
-filepath = "D:\\Data_Science\\GW-DataAnalytics\\HomeWork\\Python\\Python_Challenge\\PyBank\\budget_data_2.csv"
-   
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
 
-with open(filepath, newline="") as csvfile:
-     csvreader = csv.reader(csvfile, delimiter=",")
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as financial_data:
+    reader = csv.reader(financial_data)
 
-  #Initiatng Lists .
-     Months=[]
-     Total_No_months = []
-     Total_Revenue = []
-     Average_Revenue_Change = []
-     Greatest_Revenue_Increase = []
-     Greatest_Revenue_Decrease= []
-        
-for row in csvreader: 
-      
-      #Add months to counter
-      Months += 1
-        
-      #The total number of months .
-      Total_No_months.append(Months)
-        
-    
-      #calculating the total revenue.
-      revenue = sum(int(row[1]))
-      Total_revenue.append(revenue)
-            
-    
-      #To calculate the average revenue change looping through current and previous month
-for i in range (len(csvreader)-1):
-       #getting the current revenue
-       current= csvreader[i+1]
-       current_revenue= int(current[1])
-       # getting the previous revenue
-       previous=csvreader[i]
-       previous_revenue= int(previous[1])
-       #Computing the revenue change on monthly basis
-       revenue_change= (current_revenue-previous_revenue)
-       #computing the Average revenue
-       Average_Revenue_Change.append(revenue_change/Total_No_months)
-       
-      #The greatest increase in revenue 
-for i in range(len(csvreader)-1):
-        if (csvreader[i+1][1]-csvreader[i])==max(revenue_change):
-          Greatest_Revenue_Increase= max(revenue_change)
-          Greatest_revenue_increase_month= csvreader[i+1][0]
-      #The greatest decrease in revenue 
-        elif (csvreader[i+1][1]-csvreader[i])==min(revenue_change):
-          Greatest_Revenue_Decrease= min(revenue_change)
-          Greatest_revenue_increase_month= csvreader[i+1][0]
+    # Read the header row
+    header = next(reader)
 
-with open("Financial_Analysis.txt", "w") as text_file:
-    text_file.write("Total months: %s" % Total_No_months)
-    text_file.write("Total Revenue: %s" % Total_Revenue )
-    text_file.write("Average Revenue Change: %s" % Average_Revenue_Change )
-    text_file.write("Greatest increase in revenue: %s" % Greatest_revenue_increase_month )
-    text_file.write("Greatest Decrease in revenue: %s" % Greatest_revenue_Decrease_month )
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    total_months = total_months + 1
+    total_net = total_net + int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+
+        # Track the total
+        total_months = total_months + 1
+        total_net = total_net + int(row[1])
+
+        # Track the net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_of_change = month_of_change + [row[0]]
+
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
+
+# Generate Output Summary
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average  Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print the output (to terminal)
+print(output)
+
+# Export the results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
